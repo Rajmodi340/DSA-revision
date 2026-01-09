adfee2c3784c3cb53f9de523329e6fb7f236282a
Raj Modi <modiraj340@gmail.com>
2026-01-09 10:12:57 +0530

    reverse

--- Array/reverse.cpp
+++ Array/reverse.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
-
+void reverse(vector<int>&a){
+    int n=a.size();
+    int start=0;
+    int end=n-1;
+    while(start<end){
+        swap(a[start],a[end]);
+        start++,end--;
+    }
+}
 int main(){
-    cout<<"revrse"<<endl;
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    reverse(a);
+    for (int x : a)
+    {
+        cout << x << " ";
+    }
     return 0;
 }
