b7d339a8fb2061e5b0ac24938b25f243da9ea4f2
Raj Modi <modiraj340@gmail.com>
2026-01-09 12:17:46 +0530

    3sum

---
+++ Array/3sum.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector<vector<int>> result;
+void twosum(vector<int> &nums, int target, int i, int j)
+{
+    while (i < j)
+    {
+        if (nums[i] + nums[j] > target)
+            j--;
+        else if (nums[i] + nums[j] < target)
+            i++;
+        else
+        {
+            while (i < j && nums[i] == nums[i + 1])
+                i++;
+            while (i < j && nums[j] == nums[j - 1])
+                j--;
+            result.push_back({-target, nums[i], nums[j]});
+            i++;
+            j--;
+        }
+    }
+}
+vector<vector<int>> threeSum(vector<int> &nums)
+{
+    int n = nums.size();
+    if (n < 3)
+    {
+        return {};
+    }
+    result.clear();
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 0 && nums[i] == nums[i - 1])
+        {
+            continue;
+        }
+        int n1 = nums[i];
+        int target = -n1;
+        twosum(nums, target, i + 1, n - 1);
+    }
+    return result;
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    vector<vector<int>> ans = threeSum(a);
+    for (auto triplet : ans)
+    {
+        cout << "[ ";
+        for (int x : triplet)
+            cout << x << " ";
+        cout << "]\n";
+    }
+    return 0;
+}
